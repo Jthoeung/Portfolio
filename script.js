c8842f17be08ea2cc132b6f271343cf866f0e90c
Jada <Jthoeung@uci.edu>
2025-01-20 21:22:15 -0800

    decided to use bootstrap nav bar

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function openTab(event, tabName){
-
-    const contents = document.querySelectorAll('.tab-page');
-
-    contents.forEach(content => content.classList.remove('active'));
-
-
-    const tabs = document.querySelectorAll('.single-tab');
-    tabs.forEach(tab => tab.classList.remove('active'));
-
-
-    document.getElementById(tabName).classList.add('active');
-    event.currentTarget.classList.add('active');
-
-}
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
